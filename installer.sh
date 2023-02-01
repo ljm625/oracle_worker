6c2dd1f501ae799ee614c9deb70a9bd4bff124b6
Jiaming Li <ljm625@gmail.com>
2023-02-01 15:51:25 +0800

    fix

--- installer.sh
+++ installer.sh
@@ -1,10 +1,14 @@
 echo "Start installing Oracle Worker"
-apt install update && apt install -y wget
+apt-get update && apt-get install -y wget git build-essential
+git clone https://github.com/opsengine/cpulimit
+cd cpulimit
+make
+mv src/cpulimit /usr/bin
+cd ..
+rm -rf cpulimit
 wget https://github.com/ljm625/oracle_worker/raw/main/worker
-wget https://github.com/ljm625/oracle_worker/raw/main/cpulimit
 wget https://github.com/ljm625/oracle_worker/raw/main/worker.service
 mv ./worker /usr/bin/
-mv ./cpulimit /usr/bin/
 mv ./worker.service /etc/systemd/system/
 chmod +x /usr/bin/worker
 chmod +x /usr/bin/cpulimit
