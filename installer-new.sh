661c3d69e29801ef21fb584ab5dfc13ce3cc3da2
Jiaming Li <ljm625@gmail.com>
2023-02-02 23:36:56 +0800

    fix

---
+++ installer-new.sh
@@ -0,0 +1,11 @@
+echo "Start installing Oracle Worker"
+apt-get update && apt-get install -y wget
+wget https://github.com/ljm625/oracle_worker/raw/main/worker
+wget https://github.com/ljm625/oracle_worker/raw/main/worker_new.service
+mv ./worker /usr/bin/
+mv ./worker_new.service /etc/systemd/system/
+chmod +x /usr/bin/worker
+systemctl daemon-reload
+systemctl enable worker
+systemctl start worker
+
